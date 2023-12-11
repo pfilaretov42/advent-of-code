a9ac06e8b750e85cb4c89377f497344b24d574bd
Petr Filaretov <p.filaretov@okko.tv>
2023-12-11 10:57:25 +0300

    2023 day 3 - part 2

--- src/main/kotlin/dev/pfilaretov42/aoc/aoc2023/day3/GearRatios.kt
+++ src/main/kotlin/dev/pfilaretov42/aoc/aoc2023/day3/GearRatios.kt
@@ -4,8 +4,52 @@ import java.io.File
 
 fun main() {
     val inputFile = File("input/2023/day3/input")
-    partOne(inputFile)
-//    partTwo(inputFile)
+//    partOne(inputFile)
+    partTwo(inputFile)
+}
+
+fun partTwo(inputFile: File) {
+    var totalSum = 0
+    val asteriskIndicesLines = mutableListOf<Set<Int>>()
+    val partNumberCandidateLines = mutableListOf<List<PartNumberCandidate>>(emptyList())
+    inputFile.forEachLine { line ->
+        asteriskIndicesLines += "[*]+".toRegex()
+            .findAll(line)
+            .map {
+                it.range.first
+            }
+            .toSet()
+
+        partNumberCandidateLines += "[0-9]+".toRegex()
+            .findAll(line)
+            .map {
+                PartNumberCandidate(it.value.toInt(), IntRange(it.range.first - 1, it.range.last + 1))
+            }
+            .toList()
+    }
+
+    partNumberCandidateLines += listOf<PartNumberCandidate>()
+
+    asteriskIndicesLines.forEachIndexed { i, asteriskIndices ->
+        asteriskIndices.forEach { asteriskIndex ->
+            val candidates = partNumberCandidateLines
+                .filterIndexed { j, _ -> j == i || j == i + 1 || j == i + 2 }
+                .flatMap { partNumberCandidates ->
+                    partNumberCandidates.filter { partNumberCandidate ->
+                        partNumberCandidate.indices.contains(asteriskIndex)
+                    }
+                }
+                .takeIf { it.size == 2 }
+
+            totalSum += candidates
+                ?.map { it.value }
+                ?.reduce { acc, candidateValue -> acc * candidateValue }
+                ?: 0
+        }
+    }
+
+    println(totalSum)
+
 }
 
 fun partOne(inputFile: File) {
@@ -20,7 +64,6 @@ fun partOne(inputFile: File) {
         [ [776, (16,18)], [552, (33,35)], [...], ... ]
         [...]
     ]
-
      */
 
     var totalSum = 0
