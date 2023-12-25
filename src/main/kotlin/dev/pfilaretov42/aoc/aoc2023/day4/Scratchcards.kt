ab78a2cca35f68fa166fb6524bab2cb50d4324ff
Petr Filaretov <p.filaretov@okko.tv>
2023-12-25 11:55:19 +0300

    2023 day 4 - part 2 - draft

--- src/main/kotlin/dev/pfilaretov42/aoc/aoc2023/day4/Scratchcards.kt
+++ src/main/kotlin/dev/pfilaretov42/aoc/aoc2023/day4/Scratchcards.kt
@@ -5,8 +5,37 @@ import kotlin.math.pow
 
 fun main() {
     val inputFile = File("input/2023/day4/input")
-    partOne(inputFile)
-//    partTwo(inputFile)
+//    partOne(inputFile)
+    partTwo(inputFile)
+}
+
+fun partTwo(inputFile: File) {
+    val lineRegex = ":\\s+".toRegex()
+    val cardRegex = "\\s+\\|\\s+".toRegex()
+    val numbersRegex = "\\s+".toRegex()
+
+    val cardsCountMap = mutableMapOf<Int, Int>()
+
+    inputFile.forEachLine { line ->
+        val cardAndNumbers = line.split(lineRegex)
+
+        val cardNumber = cardAndNumbers[0].split(numbersRegex)[1].toInt()
+        val cardsCount = cardsCountMap.merge(cardNumber, 1) { oldValue, _ -> oldValue + 1 }!!
+
+        val numbers = cardAndNumbers[1].split(cardRegex)
+        val winningNumbers = numbers[0].split(numbersRegex).toSet()
+        val numbersToTest = numbers[1].split(numbersRegex).toSet()
+
+        val winningCount = numbersToTest.filter { winningNumbers.contains(it) }.size
+
+        IntRange(cardNumber + 1, cardNumber + winningCount).forEach { additionalCardNumber ->
+            cardsCountMap.merge(additionalCardNumber, 1) { oldValue, _ -> oldValue + cardsCount }
+        }
+
+        println(cardsCountMap.values.sum())
+    }
+
+    println(cardsCountMap.values.sum())
 }
 
 fun partOne(inputFile: File) {
